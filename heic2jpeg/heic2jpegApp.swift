32cffb0ab97953dbfb06121af14e49ad0d3f1ea6
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-04-05 18:47:31 -0500

    Initial Commit

---
+++ heic2jpeg/heic2jpegApp.swift
@@ -0,0 +1,17 @@
+//
+//  heic2jpegApp.swift
+//  heic2jpeg
+//
+//  Created by Nathaniel Bedggood on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct heic2jpegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ heic2jpegTests/heic2jpegTests.swift
@@ -0,0 +1,17 @@
+//
+//  heic2jpegTests.swift
+//  heic2jpegTests
+//
+//  Created by Nathaniel Bedggood on 05/04/2025.
+//
+
+import Testing
+@testable import heic2jpeg
+
+struct heic2jpegTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
