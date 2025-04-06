8b02d460ce56677cab0575ff1f39726bd433ec37
Nathaniel Bedggood <ngbedggood@gmail.com>
2025-04-06 15:44:30 -0500

    Displaying a selected image

--- heic2jpeg/ContentView.swift
+++ heic2jpeg/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import CoreImage
+import PhotosUI
 
 struct ContentView: View {
+    
+    let context = CIContext()
+    
+    @State private var selectedData: PhotosPickerItem? = nil
+    @State private var selectedImage: UIImage? = nil
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let image = selectedImage {
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 300)
+                        } else {
+                            Text("No image selected")
+                                .foregroundColor(.gray)
+                        }
+            
+            PhotosPicker(selection: $selectedData, matching: .images) {
+                Text("Select Photos")
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
+        .onChange(of: selectedData) { _, newItem in
+                    Task {
+                        if let data = try? await newItem?.loadTransferable(type: Data.self),
+                        let uiImage = UIImage(data: data) {
+                            selectedImage = uiImage
+                        } else {
+                            print("Failed to load image data.")
+                        }
+                    }
+                }
         .padding()
+
+        
+        
     }
 }
 
